84193b5c30a105b76a0a76ce94fdd20816bd52af
Taea Vogel <taelxvie@gmail.com>
2024-02-23 10:25:48 -0500

    Add under construction msg and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  const hello = api.post.hello.useQuery({ text: "Thank you for visiting. This website is under construction." });
 
   return (
     <>
@@ -14,12 +13,32 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Taea Vogel
           </h1>
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+          </p>
+          <p>
+            <a
+              href="https://www.linkedin.com/in/taea"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-2xl text-white underline"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <p>
+            <a
+              href="https://www.github.com/adrastaea"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-2xl text-white underline"
+            >
+              GitHub
+            </a>
           </p>
         </div>
       </main>
